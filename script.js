73053983635b7dcd06ed5e1619826e687686e789
IcaroD <icaro.davila.santos@gmail.com>
2024-07-28 15:28:30 -0300

    Update

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 function enviarMensagem() {
-    let inputMensagem = document.querySelector("#inputMensagem");
-    let mensagem = inputMensagem.value.trim();
+  let inputMensagem = document.querySelector("#inputMensagem");
+  let mensagem = inputMensagem.value.trim();
 
-    if (mensagem == "") {
-        alert("Nenhuma mensagem!!!")
-        return;
-    }
+  if (mensagem == "") {
+    alert("Nenhuma mensagem!!!")
+    return;
+  }
   
-    let areaDeMensagens = document.querySelector(".areaDeMensagens");
+  let areaDeMensagens = document.querySelector(".areaDeMensagens");
   
-    let novaMensagem = document.createElement("div");
-    novaMensagem.className = "mensagens";
+  let novaMensagem = document.createElement("div");
+  novaMensagem.className = "mensagens";
   
-    novaMensagem.innerHTML = `
-      <div class="mensagem">
-        <div class="texto">
-          ${mensagem}
-        </div>
-        <div class="btnMensagem">
-          <button class="editar" onclick="editarMensagem(this)">Editar</button>
-          <button class="excluir" onclick="excluirMensagem(this)">Excluir</button>
-        </div>
+  novaMensagem.innerHTML = `
+    <div class="mensagem">
+      <div class="texto">
+        ${mensagem}
       </div>
-    `;
+      <div class="btnMensagem">
+        <button class="editar" onclick="editarMensagem(this)">Editar</button>
+        <button class="excluir" onclick="excluirMensagem(this)">Excluir</button>
+      </div>
+    </div>
+  `;
   
-    areaDeMensagens.appendChild(novaMensagem);
+  areaDeMensagens.appendChild(novaMensagem);
   
-    inputMensagem.value = "";
+  inputMensagem.value = "";
 }
 
 function excluirMensagem(botaoExcluir) {
-    let mensagemParaExcluir = botaoExcluir.closest(".mensagens");
-    mensagemParaExcluir.remove();
+  let mensagemParaExcluir = botaoExcluir.closest(".mensagens");
+  mensagemParaExcluir.remove();
 }
   
 function editarMensagem(botaoEditar) {
-    let mensagemParaEditar = botaoEditar.closest(".mensagens");
-    let textoMensagem = mensagemParaEditar.querySelector(".texto");
-    let novoTexto = prompt("Digite o novo texto:");
+  let mensagemParaEditar = botaoEditar.closest(".mensagens");
+  let textoMensagem = mensagemParaEditar.querySelector(".texto");
+  let novoTexto = prompt("Digite o novo texto:");
     
-    if (novoTexto !== null) {
-      textoMensagem.textContent = novoTexto;
-    }
+  if (novoTexto !== null) {
+    textoMensagem.textContent = novoTexto;
+  }
 }
   
   
 document.addEventListener("DOMContentLoaded", function() {
-    let enviar = document.querySelector("#Enviar");
-    enviar.addEventListener("click", enviarMensagem);
+  let enviar = document.querySelector("#Enviar");
+  enviar.addEventListener("click", enviarMensagem);
 });
   
